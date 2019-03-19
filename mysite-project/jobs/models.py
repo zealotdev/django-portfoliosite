8df0bb5c4888f6098182ddbaf553f7efc4df36e2
Jorge <g33ky.me@pm.me>
2019-03-19 12:56:52 +0300

    env setup for the project

--- mysite-project/jobs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Job(models.Model):
-    title = models.CharField(max_length=20)
-    image = models.ImageField(upload_to="images/")
-    summary = models.TextField()
-
-    def __str__(self):
-        return self.title
